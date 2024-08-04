6cb00470f2c9653f03b5b72a017cdf57ff885e61
Raggi <35541698+ragrag@users.noreply.github.com>
2024-08-04 15:42:31 +0200

    Update bench.ts

--- bench/bench.ts
+++ bench/bench.ts
@@ -1,4 +1,4 @@
-import { setTimeout } from 'timers/promises';
+import { setTimeout } from 'node:timers/promises';
 import { Yakugen, type YakugenOptions } from '../src/yakugen';
 
 function fibbonaci(n: number): number {
@@ -14,20 +14,6 @@ async function simulateTask(): Promise<number> {
     return fibbonaci(30);
 }
 
-async function runWithFixedBatchSize(tasks: Array<() => Promise<number>>, batchSize: number): Promise<number> {
-    const start = Date.now();
-    const results: number[] = [];
-
-    for (let i = 0; i < tasks.length; i += batchSize) {
-        const batch = tasks.slice(i, i + batchSize);
-        const batchResults = await Promise.all(batch.map(task => task()));
-        results.push(...batchResults);
-    }
-
-    const end = Date.now();
-    return end - start;
-}
-
 async function runWithYakugen(tasks: Array<() => Promise<number>>, options?: YakugenOptions): Promise<number> {
     const start = Date.now();
     await Yakugen.all(tasks, options);
@@ -39,20 +25,11 @@ async function runTest() {
     const numTasks = 10_000;
     const tasks = Array.from({ length: numTasks }, () => async () => simulateTask());
 
-    console.log(`Running ${numTasks} tasks...`);
-
-    const batchSizes = [500, 1000, 1500];
-    for (const batchSize of batchSizes) {
-        const duration = await runWithFixedBatchSize(tasks, batchSize);
-        console.log(`Fixed batch size ${batchSize}: ${duration}ms`);
-    }
-
-    const yakugenDuration = await runWithYakugen(tasks, {
+    await runWithYakugen(tasks, {
         onProgress: (_, metricsSnapshot, concurrency) => {
-            console.log(concurrency, metricsSnapshot);
+            console.log({ concurrency, ...metricsSnapshot });
         },
     });
-    console.log(`Yakugen: ${yakugenDuration}ms`);
 }
 
 runTest().catch(console.error);
